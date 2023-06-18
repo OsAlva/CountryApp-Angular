507b92f383a1d67c7f625687fd3a69e202de41df
Oscar Alva <oscaraldrin23@gmail.com>
2023-06-18 14:00:58 +0200

    countries services - rescountries

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
 export class ServiceNameService {
-    constructor(private httpClient: HttpClient) { }
+
+    private apiurl = 'https://restcountries.com/v3.1/';
+
+
+    constructor(private http: HttpClient) { }
+
+     searchCapital(term: string): Observable<Country[]> {
+        const url =`${this.apiurl}/capital/${term}`;
+        //retorno un observable de tipo country pero no estoy suscrito a el por eso me tengo que subscribir en el componente
+            return this.http.get<Country[]>(url); 
+     }
     
 }
